15339ac6da799f0dd4e18418b92e7a2627c1a0b7
unknown <you@example.com>
2025-10-21 22:44:16 +0600

    admin routes-protection Done

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -2,19 +2,22 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest.js';
 import { AdminControllers } from './admin.controller.js';
 import { updateAdminValidationSchema } from './admin.validation.js';
+import auth from '../../middlewares/auth.js';
+import { USER_ROLE } from '../user/user.constant.js';
 
 const router = express.Router();
 
-router.get('/', AdminControllers.getAllAdmins);
+router.get('/', auth(USER_ROLE.superAdmin, USER_ROLE.admin) , AdminControllers.getAllAdmins);
 
-router.get('/:id', AdminControllers.getSingleAdmin);
+router.get('/:id', auth(USER_ROLE.superAdmin, USER_ROLE.admin) , AdminControllers.getSingleAdmin);
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin),
   validateRequest(updateAdminValidationSchema),
   AdminControllers.updateAdmin
 );
 
-router.delete('/:adminId', AdminControllers.deleteAdmin);
+router.delete('/:adminId', auth(USER_ROLE.superAdmin) ,AdminControllers.deleteAdmin);
 
 export const AdminRoutes = router;
